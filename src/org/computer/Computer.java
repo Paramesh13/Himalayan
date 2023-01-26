89e41bb9e279409e5b32703400591f4d328ea742
paramesh13 <bharathjp1013@gmail.com>
2023-01-26 14:24:56 +0530

    sucess

---
+++ src/org/computer/Computer.java
@@ -0,0 +1,15 @@
+package org.computer;
+
+public class Computer {
+	public static void main(String[] args) {
+		
+		int a=10 , b=20;
+	    System.out.println(a>b?a:b);
+	    System.out.println("super");
+	    
+	    
+	    
+	}
+	
+	}
+
